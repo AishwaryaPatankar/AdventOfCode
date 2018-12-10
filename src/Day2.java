978efe535de7b0bb3dcee72b22d155f9093200dc
AishwaryaPatankar <patankaraishwarya@yahoo.com>
2018-12-09 23:34:42 -0600

    Day 2 challenge 2 completed

--- src/Day2.java
+++ src/Day2.java
@@ -8,7 +8,8 @@ public class Day2 {
     }
 
     public static void challenge2() {
-//        System.out.printf("Day 1: Challenge 2 completed. \n \t First repeated total frequency is %d \n", repeated);
+        String commonLetters = findCommonLettersOfNamesOffByOne(Constants.boxNames);
+        System.out.printf("Day 1: Challenge 2 completed. \n \t The common letters are %s \n", commonLetters);
     }
 
     private static Integer calculateCheckSum(String boxNames) {
@@ -41,4 +42,38 @@ public class Day2 {
         }
         return twos * threes;
     }
+
+
+
+    private static String findCommonLettersOfNamesOffByOne(String names){
+        //This will only work if all the boxes have names with the same number of letters
+        String[] nameArray = names.split("\n");
+        int numLetters = nameArray[0].length();
+        int arrayCounter=0;
+
+        String returnString = "";
+
+        for (int i=0; i<numLetters; i++){
+            for(arrayCounter=0; arrayCounter<nameArray.length; arrayCounter++){
+                String currentMinusLetter = minusChar(nameArray[arrayCounter], i);
+                for(int secondArrayCounter=arrayCounter+1; secondArrayCounter<nameArray.length; secondArrayCounter++){
+                    String secondCurrentMinusLetter = minusChar(nameArray[secondArrayCounter],i);
+                    if (currentMinusLetter.equals(secondCurrentMinusLetter)){
+                        returnString = currentMinusLetter;
+
+                    }
+                }
+
+
+            }
+        }
+
+
+        return returnString;
+    }
+
+    private static String minusChar (String str, int i){
+        return str.substring(0, i) + str.substring(i + 1, str.length());
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         Day1.challenge1();
         Day1.challenge2();
         Day2.challenge1();
+        Day2.challenge2();
     }
 }
